77f52758f819c452679b0dba2fc3e25878e022fd
EstebanCR31 <143008597+EstebanCR31@users.noreply.github.com>
2026-06-13 07:51:32 -0500

    Prueba de duplicidad larga

--- src/main/java/com/equipopfqa/calculadora/Calculadora.java
+++ src/main/java/com/equipopfqa/calculadora/Calculadora.java
@@ -6,24 +6,49 @@ public class Calculadora {
         return primerNumero + segundoNumero;
     }
 
-    public double sumar2(double primerNumero, double segundoNumero) {
-        return primerNumero + segundoNumero;
+    public double metodoLargo1(double a, double b) {
+        double temp = a + b;
+        temp = temp * 2;
+        temp = temp - 1;
+        temp = temp / 2;
+        temp = temp + 5;
+        temp = temp * 3;
+        temp = temp - 4;
+        temp = temp / 3;
+        temp = temp + 10;
+        temp = temp * 4;
+        temp = temp - 8;
+        return temp;
     }
 
-    public double sumar3(double primerNumero, double segundoNumero) {
-        return primerNumero + segundoNumero;
+    public double metodoLargo2(double a, double b) {
+        double temp = a + b;
+        temp = temp * 2;
+        temp = temp - 1;
+        temp = temp / 2;
+        temp = temp + 5;
+        temp = temp * 3;
+        temp = temp - 4;
+        temp = temp / 3;
+        temp = temp + 10;
+        temp = temp * 4;
+        temp = temp - 8;
+        return temp;
     }
 
-    public double sumar4(double primerNumero, double segundoNumero) {
-        return primerNumero + segundoNumero;
-    }
-
-    public double sumar5(double primerNumero, double segundoNumero) {
-        return primerNumero + segundoNumero;
-    }
-
-    public double sumar6(double primerNumero, double segundoNumero) {
-        return primerNumero + segundoNumero;
+    public double metodoLargo3(double a, double b) {
+        double temp = a + b;
+        temp = temp * 2;
+        temp = temp - 1;
+        temp = temp / 2;
+        temp = temp + 5;
+        temp = temp * 3;
+        temp = temp - 4;
+        temp = temp / 3;
+        temp = temp + 10;
+        temp = temp * 4;
+        temp = temp - 8;
+        return temp;
     }
 
     public double restar(double primerNumero, double segundoNumero) {
