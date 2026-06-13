3b7eb5e376caa40c9272f1636a487048a5998aa6
EstebanCR31 <143008597+EstebanCR31@users.noreply.github.com>
2026-06-13 09:04:57 -0500

    Pureba

--- src/main/java/com/equipopfqa/calculadora/Calculadora.java
+++ src/main/java/com/equipopfqa/calculadora/Calculadora.java
@@ -21,6 +21,66 @@ public class Calculadora {
         return temp;
     }
 
+        public double metodoLargo2(double a, double b) {
+        double temp = a + b;
+        temp = temp * 2;
+        temp = temp - 1;
+        temp = temp / 2;
+        temp = temp + 5;
+        temp = temp * 3;
+        temp = temp - 4;
+        temp = temp / 3;
+        temp = temp + 10;
+        temp = temp * 4;
+        temp = temp - 8;
+        return temp;
+    }
+
+        public double metodoLargo3(double a, double b) {
+        double temp = a + b;
+        temp = temp * 2;
+        temp = temp - 1;
+        temp = temp / 2;
+        temp = temp + 5;
+        temp = temp * 3;
+        temp = temp - 4;
+        temp = temp / 3;
+        temp = temp + 10;
+        temp = temp * 4;
+        temp = temp - 8;
+        return temp;
+    }
+
+        public double metodoLargo4(double a, double b) {
+        double temp = a + b;
+        temp = temp * 2;
+        temp = temp - 1;
+        temp = temp / 2;
+        temp = temp + 5;
+        temp = temp * 3;
+        temp = temp - 4;
+        temp = temp / 3;
+        temp = temp + 10;
+        temp = temp * 4;
+        temp = temp - 8;
+        return temp;
+    }
+
+        public double metodoLargo5(double a, double b) {
+        double temp = a + b;
+        temp = temp * 2;
+        temp = temp - 1;
+        temp = temp / 2;
+        temp = temp + 5;
+        temp = temp * 3;
+        temp = temp - 4;
+        temp = temp / 3;
+        temp = temp + 10;
+        temp = temp * 4;
+        temp = temp - 8;
+        return temp;
+    }
+
     public double restar(double primerNumero, double segundoNumero) {
         return primerNumero - segundoNumero;
     }
